ec2d9204c8f299d8922c7440ab3c65cc183ceae7
Marvin Matovu <marvinmatovu@gmail.com>
2019-09-29 22:26:40 +0100

    refactored for length of 2

--- Reversing/lib/reverse.js
+++ Reversing/lib/reverse.js
@@ -1,11 +1,14 @@
 const reverse = (str) => {
-    if(str == 'ab') {
-        return "ba";
-    } else if(str == 'cd') {
-        return 'dc';
-    } else if(str == 'ef') {
-        return 'fe';
+    if(str.length > 1) {
+        return str[1] + str[0];
     }
+    // if(str == 'ab') {
+    //     return "ba";
+    // } else if(str == 'cd') {
+    //     return 'dc';
+    // } else if(str == 'ef') {
+    //     return 'fe';
+    // }
     return str;
 };
 
